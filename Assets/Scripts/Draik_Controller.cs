fbe18744fb4bb2f5f53794051ab3cf75838ed44d
babyboy8100 <lpleites@gmail.com>
2017-05-26 22:52:14 -0700

    Initial commit

---
+++ Assets/Scripts/Draik_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Draik_Controller : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+    public float draikJumpforce = 500f;
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+            myRigidBody.AddForce(transform.up * draikJumpforce);
+        }
+    }
+}
