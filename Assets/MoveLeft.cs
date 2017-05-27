cefe8df842bf1e834dde43a08593e7f58b0c9311
babyboy8100 <lpleites@gmail.com>
2017-05-27 02:21:14 -0700

    1.2
    Made animations for the Zombies.
    made them go left on the screen.
    next is the draik death animation plus other additional level tweaks.

---
+++ Assets/MoveLeft.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MoveLeft : MonoBehaviour {
+
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.left * speed*Time.deltaTime;
+	}
+}
--- Assets/Scripts/Draik_Controller.cs
+++ Assets/Scripts/Draik_Controller.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Draik_Controller : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
+    private Animator myAnim;
     public float draikJumpforce = 500f;
     // Use this for initialization
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,5 +19,15 @@ public class Draik_Controller : MonoBehaviour {
         {
             myRigidBody.AddForce(transform.up * draikJumpforce);
         }
+        myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
 }
